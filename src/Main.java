80c20b18f41371d6518072bee60e4412e04f3d99
Tata <lentstata@gmail.com>
2023-03-29 15:11:37 +0500

    Задание 2 по лямбде

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,23 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
         List<Person> heroes = new ArrayList<>();
         heroes.add(new Person("Дункан", "Айдахо", 45));
         heroes.add(new Person("Джонатан", "Стрендж", 33));
+        heroes.add(new Person("Люси", "Карлайл", 16));
         heroes.add(new Person("Федерико", "де Сойя", 44));
         heroes.add(new Person("Артур", "Дент", 32));
         heroes.add(new Person("Энтони", "Локвуд", 15));
 
         Collections.sort(heroes, new ComparatorWhoIsMoreNotable());
+        System.out.println(heroes);
+
+        Predicate<Person> personPredicate = person -> person.getAge() < 18;
+        heroes.removeIf(personPredicate);
         System.out.println(heroes);
     }
 }
