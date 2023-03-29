89a072b6f95e71f5ac1a15c5ea1b8a0cd566003f
Tata <lentstata@gmail.com>
2023-03-29 14:56:55 +0500

    Задание 1 по лямбде

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,27 @@
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Person> heroes = new ArrayList<>();
+        List<Person> heroes = new ArrayList<Person>();
         heroes.add(new Person("Дункан", "Айдахо", 45));
         heroes.add(new Person("Джонатан", "Стрендж", 33));
         heroes.add(new Person("Федерико", "де Сойя", 44));
         heroes.add(new Person("Артур", "Дент", 32));
         heroes.add(new Person("Энтони", "Локвуд", 15));
 
-        Collections.sort(heroes, new ComparatorWhoIsMoreNotable());
+
+        heroes.sort((o1, o2) -> {
+            String[] wordsofSurnameOne = new String[o1.getSurname().split(" ").length];
+            String[] wordsofSurnameTwo = new String[o2.getSurname().split(" ").length];
+            if (wordsofSurnameOne.length > wordsofSurnameTwo.length) {
+                return Integer.compare(o1.getSurname().length(), o2.getSurname().length());
+            } else if (o1.getAge() > o2.getAge()) {
+                return Integer.compare(o1.getAge(), o2.getAge());
+            } else {
+                return -1;
+            }
+        });
         System.out.println(heroes);
     }
 }
